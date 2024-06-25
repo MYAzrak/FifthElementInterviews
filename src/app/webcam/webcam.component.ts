edd4f9dfee2d652c2c32ee8a1999cd504afd9cc9
MohammadYA <yaserazrak5@gmail.com>
2024-06-25 13:33:02 +0400

    Fixed the webcam component name :)

---
+++ src/app/webcam/webcam.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-webcam',
+  standalone: true,
+  imports: [],
+  templateUrl: './webcam.component.html',
+  styleUrl: './webcam.component.css'
+})
+export class WebcamComponent {
+
+}
