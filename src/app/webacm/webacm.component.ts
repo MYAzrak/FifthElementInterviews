7bc36affe75e52f02c7d0a8f3403d45b57bee9c2
MohammadYA <yaserazrak5@gmail.com>
2024-06-25 13:10:38 +0400

    Added a webcam component

---
+++ src/app/webacm/webacm.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-webacm',
+  standalone: true,
+  imports: [],
+  templateUrl: './webacm.component.html',
+  styleUrl: './webacm.component.css'
+})
+export class WebacmComponent {
+
+}
