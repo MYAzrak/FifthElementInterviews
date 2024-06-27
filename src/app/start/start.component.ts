885a59ba353cae51b52d915ca74b88204a90131a
MohammadYA <yaserazrak5@gmail.com>
2024-06-27 15:15:15 +0400

    Fixed bug (template -> templateUrl in start comp.)

--- src/app/start/start.component.ts
+++ src/app/start/start.component.ts
@@ -1,10 +1,12 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
 @Component({
   selector: 'app-start',
   standalone: true,
-  imports: [CommonModule],
-  template: './start.component.html',
+  imports: [CommonModule, RouterModule],
+  templateUrl: './start.component.html',
   styleUrl: './start.component.css',
+
 })
 export class StartComponent {}
