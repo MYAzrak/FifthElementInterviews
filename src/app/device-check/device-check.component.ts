75f35831895346e748f47fe795724454cfe7e4e6
MohammadYA <yaserazrak5@gmail.com>
2024-07-23 15:25:49 +0400

    Generated the device-check component

---
+++ src/app/device-check/device-check.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-device-check',
+  standalone: true,
+  imports: [],
+  templateUrl: './device-check.component.html',
+  styleUrl: './device-check.component.css'
+})
+export class DeviceCheckComponent {
+
+}
