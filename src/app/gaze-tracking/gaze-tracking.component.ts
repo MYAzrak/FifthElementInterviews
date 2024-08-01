ece259badb2c1d03d561314e8fa4efc5d433ea73
MohammadYA <yaserazrak5@gmail.com>
2024-08-01 16:34:59 +0400

    Generated the gaze-tracking component

---
+++ src/app/gaze-tracking/gaze-tracking.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-gaze-tracking',
+  standalone: true,
+  imports: [],
+  templateUrl: './gaze-tracking.component.html',
+  styleUrl: './gaze-tracking.component.css'
+})
+export class GazeTrackingComponent {
+
+}
