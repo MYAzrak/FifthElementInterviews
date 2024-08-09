5aa00909b1d3ea11b7c60f8856462e08647018e3
MohammadYA <yaserazrak5@gmail.com>
2024-08-09 14:28:09 +0400

    Generated the ID verification component

---
+++ src/app/id-verification/id-verification.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-id-verification',
+  standalone: true,
+  imports: [],
+  templateUrl: './id-verification.component.html',
+  styleUrl: './id-verification.component.css'
+})
+export class IdVerificationComponent {
+
+}
